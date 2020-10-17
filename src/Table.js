4f94e4363d8b46a729aeb1e5dd00b0850116dbde
Navin Kumar <navinkumar0299@outlook.com>
2020-10-17 11:47:50 +0530

    feat: getwork dashboard

---
+++ src/Table.js
@@ -0,0 +1,50 @@
+import React, { Fragment, useState, useEffect } from 'react';
+import { DataGrid } from '@material-ui/data-grid';
+import axios from 'axios';
+import './Table.css';
+
+const columns = [
+  { field: 'id', headerName: 'ID', width: 70 },
+  { field: 'employee_name', headerName: 'Employee Name', width: 140 },
+  { field: 'employee_salary', headerName: 'Employee Salary', width: 150 },
+  { field: 'employee_age', headerName: 'Employee Age', width: 130 },
+  { field: 'employee_age', headerName: 'Age', type: 'number', width: 90 }
+];
+
+function Table() {
+    
+    const [row, setRow] = useState({ data: []});
+   
+    useEffect(() => {
+      const fetchData = async () => {
+        const result = await axios('http://dummy.restapiexample.com/api/v1/employees');
+        
+        setRow(result.data);
+
+      };
+   
+      fetchData();
+
+    }, []);
+
+    const [query, setQuery] = useState('');
+
+    const onClick = () => {
+      let results = []
+      for(let i=0; i<row.data.length; i++) {
+        for(key in row.data[i]) {
+          if(row.data[i][key].indexOf(query)!=-1) {
+            results.push(row.data[i]);
+          }
+        }
+      }
+    }
+  
+   return (
+    <div style={{ height: 400, width: '100%' }} className="margin-50">
+      <DataGrid rows={row.data} columns={columns} pageSize={5} checkboxSelection />
+    </div>
+   );
+  }
+
+export default Table;
